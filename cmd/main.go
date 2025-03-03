3e3ad912c65866d50b03f3a5cbfb6158504cb48a
chnmk <a2molchanov@ya.ru>
2025-03-03 21:49:21 +0300

    chore: add basic project structure

---
+++ cmd/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "github.com/chnmk/music-library-microservice/internal/app"
+
+// Вызов функции из internal
+func main() {
+	app.Run()
+}
---
+++ internal/app/app.go
@@ -0,0 +1,8 @@
+package app
+
+import "fmt"
+
+// Запуск сервера
+func Run() {
+	fmt.Println("Hi")
+}
